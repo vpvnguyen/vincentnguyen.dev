3a99e25688123773e81cbe08619498101323d02e
Vincent Nguyen <vpvnguyen@gmail.com>
2020-10-18 14:34:34 -0700

    add favicon to helmet

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,6 +7,12 @@ const Header = ({ author }) => (
     <CssBaseline />
     <Helmet author={author}>
       <link
+        rel="icon"
+        type="image/png"
+        href="https://s3.us-east-2.amazonaws.com/vincentnguyen.dev-images/dev-icon.png"
+        sizes="16x16"
+      />
+      <link
         href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap"
         rel="stylesheet"
       />
