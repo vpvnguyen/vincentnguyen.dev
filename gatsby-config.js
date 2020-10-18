2b5a572cac3773da3a8f93a9ca3ef23c0f0fc48c
Vincent Nguyen <vpvnguyen@gmail.com>
2020-10-18 14:40:42 -0700

    remove gatsby file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,13 +51,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
