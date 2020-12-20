fbc43223fa8dcdf6955013ad869f62bc7e3a9bcc
Vincent Nguyen <vpvnguyen@gmail.com>
2020-12-20 10:33:11 -0800

    implemented StyleProps types

--- src/ui/ProjectTheme.ts
+++ src/ui/ProjectTheme.ts
@@ -1,36 +1,37 @@
 import Theme from "./Theme";
+import { StyleProps } from "./Theme";
 
 class ProjectTheme extends Theme {
-  public static container = () => ({
+  public static container = (): StyleProps => ({
     display: "flex",
     flexDirection: "column",
   });
 
-  public static buttonContainer = () => ({
+  public static buttonContainer = (): StyleProps => ({
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
     fontFamily: Theme.font().fontFamily,
   });
 
-  public static sectionLeft = () => ({
+  public static sectionLeft = (): StyleProps => ({
     display: "flex",
     flexDirection: "column",
   });
 
-  public static sectionRight = () => ({
+  public static sectionRight = (): StyleProps => ({
     justifyContent: "flex-end",
   });
 
-  public static header = () => Theme.h1();
+  public static header = (): StyleProps => Theme.h1();
 
-  public static title = () => Theme.h3();
+  public static title = (): StyleProps => Theme.h3();
 
-  public static description = () => ({
+  public static description = (): StyleProps => ({
     fontSize: ".7rem",
   });
 
-  public static date = () => ({
+  public static date = (): StyleProps => ({
     fontSize: ".6rem",
     paddingTop: "1rem",
     fontStyle: "oblique",
