1587a640156e75f0ab4c25089874ffdf349da8ff
vpvnguyen <vpvnguyen@gmail.com>
2021-02-15 12:23:22 -0800

    moved styling to background component

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,26 +1,6 @@
 import React, { FunctionComponent } from "react";
-import LayoutComponent from "../../ui/layout/Layout.component";
 import Welcome from "./Welcome";
-import Theme from "../../ui/Theme";
 
-const style: any = {
-  landingContainer: {
-    color: Theme.color().light,
-    padding: "10rem 0rem 15rem 0rem",
-  },
-  landingMessage: {
-    padding: "5rem",
-  },
-};
-
-const Landing: FunctionComponent = () => (
-  <LayoutComponent>
-    <div style={style.landingContainer}>
-      <div style={style.landingMessage}>
-        <Welcome />
-      </div>
-    </div>
-  </LayoutComponent>
-);
+const Landing: FunctionComponent = () => <Welcome />;
 
 export default Landing;
