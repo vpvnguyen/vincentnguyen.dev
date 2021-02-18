a320f7685434edcb0b08ae51043949778ece2b58
vpvnguyen <vpvnguyen@gmail.com>
2021-02-17 20:47:44 -0800

    create three dots loader

---
+++ src/components/common/Loader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Loader from "react-loader-spinner";
+import Theme from "../../ui/Theme";
+
+export const LoaderThreeDots = () => (
+  <Loader
+    type="ThreeDots"
+    color={Theme.color().accent}
+    height={80}
+    width={80}
+  />
+);
